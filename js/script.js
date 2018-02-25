f6b44d7119c592fbbc0cffa714d518f2b8ed539f
mnap <learn@10g.pl>
2018-02-25 23:14:41 +0100

    Add while loop

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 // script.js
+/* eslint-disable no-console */
 
-var a = prompt('Enter triangle base:'),
-    h = prompt('Enter triangle height'),
-    triangleArea = a * h / 2;
+var a = prompt('Enter triangle base:');
+while ( !(a > 0) ) {
+    a = prompt('Only positive number is valid value ' +
+        'Please enter a valid value:');
+}
+
+var h = prompt('Enter triangle height');
+while ( (h <= 0) || (isNaN(h) == true) ) {
+    h = prompt('Only positive number is valid value ' +
+        'Please enter a valid value:');
+}
+
+var triangleArea = a * h / 2;
 
 console.log('Triangle field with base a: ' + a +
     ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
